dd12cdea87a498fa84ddb936dffad6590fa7058b
jasminel0511 <jasmineluo0511@gmail.com>
2020-04-07 14:58:13 -0500

    teehee

---
+++ app/src/main/java/com/example/washugains/DataClass/Food.kt
@@ -0,0 +1,8 @@
+package com.example.washugains.DataClass
+
+data class Food (val name:String,
+                 val calories:Int,
+                 val carb:Float,
+                 val fat:Float,
+                 val sugars:Float,
+                 val protein:Float)
