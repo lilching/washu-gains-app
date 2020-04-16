6d7c7fdd655429cbd8d41dd71846218d9ed254d0
benAnn <bhann@wustl.edu>
2020-04-16 00:27:19 -0500

    quotes

--- app/src/main/java/com/example/washugains/Activity/WelcomePage.kt
+++ app/src/main/java/com/example/washugains/Activity/WelcomePage.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.washugains.Activity.BottomTabs.AddPage
@@ -16,10 +17,13 @@ import kotlinx.android.synthetic.main.welcome_page.*
 
 class WelcomePage : AppCompatActivity() {
 
+
+
   //  private lateinit var db : FirebaseFirestore
     private lateinit var infoButton : Button
     private lateinit var progressButton : Button
     private lateinit var addButton : Button
+    private lateinit var inspireQuote : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +34,20 @@ class WelcomePage : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
       //  val dailyInfoList=intent.getParcelableArrayListExtra<DailyInfo>("dailyInfoList")
+
+        var quoteList = ArrayList<String>()
+        //Walt Disney
+        quoteList.add("The way to get started is to quit talking and begin doing")
+        //Winston Churchill
+        quoteList.add("The pessimist sees difficulty in every opportunity. The optimist sees the " +
+                "opportunity in every difficulty")
+        //Will Rogers
+        quoteList.add("Don't let yesterday take up too much of today")
+        //Vince Lombardi
+        quoteList.add("It's not whether you get knocked down, it's whether you get up")
+        //Johann Wolfgang Von Goethe
+        quoteList.add("Knowing is not enough, we must apply. Wishing is not enough; we must do")
+
 
         val username=intent?.getStringExtra("username")
         val calories=intent?.getStringExtra("calories")
@@ -58,5 +76,10 @@ class WelcomePage : AppCompatActivity() {
             val intent = Intent(this, AddPage::class.java)
             startActivity(intent)
         }
+
+        inspireQuote = inspirationalQuote
+        inspireQuote.text = quoteList.random()
+
+
     }
 }
