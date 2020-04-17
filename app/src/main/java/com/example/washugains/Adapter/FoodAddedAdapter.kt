502b1c5f18da551423b50f73eca9cd9b1a08e156
jasminel0511 <jasmineluo0511@gmail.com>
2020-04-17 15:19:15 -0500

    foodadded adapter

--- app/src/main/java/com/example/washugains/Adapter/FoodAddedAdapter.kt
+++ app/src/main/java/com/example/washugains/Adapter/FoodAddedAdapter.kt
@@ -1,132 +1,50 @@
 package com.example.washugains.Adapter
 
-import android.os.Build
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Filter
-import android.widget.Filterable
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
-import com.example.washugains.DataClass.DailyInfo
 import com.example.washugains.DataClass.Food
-import com.example.washugains.ExpandableLayout
 import com.example.washugains.R
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.expandable_food_row.view.*
-import java.time.LocalDate
-import java.util.*
+
 import kotlin.collections.ArrayList
 
 class FoodAddedViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
-    RecyclerView.ViewHolder(inflater.inflate(R.layout.expandable_food_row, parent, false)) {
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.added_food_row, parent, false)) {
 
-    private val name: TextView = itemView.findViewById(R.id.foodAddedNameText)
-    private val calories: TextView =itemView.findViewById(R.id.caloriesAddedText)
-    private val carbs: TextView =itemView.findViewById(R.id.carbsAddedText)
-    private val protein: TextView =itemView.findViewById(R.id.proteinAddedText)
-    private val sugar: TextView =itemView.findViewById(R.id.sugarAddedText)
-    private val fat: TextView =itemView.findViewById(R.id.fatAddedText)
-    private val foodButton: Button =itemView.findViewById(R.id.deleteFoodButton)
+    private val name: TextView = itemView.findViewById(R.id.foodNameText)
+    private val calories: TextView=itemView.findViewById(R.id.caloriesText)
+    private val carbs: TextView=itemView.findViewById(R.id.carbsText)
+    private val protein: TextView=itemView.findViewById(R.id.proteinText)
+    private val sugar: TextView=itemView.findViewById(R.id.sugarText)
+    private val fat: TextView=itemView.findViewById(R.id.fatText)
+    private val deleteButton:Button=itemView.findViewById(R.id.deleteFoodButton)
 
-
-    fun bind(food : Food, db: FirebaseFirestore, currentDate:String, uid:String?){
+    fun bind(food : Food){
         name.text=food.name
         calories.text="Calories: "+food.calories
         carbs.text="Carbs: "+food.carb+"g"
         protein.text="Protein: "+food.protein+"g"
         sugar.text="Sugar: "+food.sugars+"g"
         fat.text="Fat: "+food.fat+"g"
-        if (uid!=null) {
-            foodButton.setOnClickListener {
-                db.collection("users").document(uid).collection("dates")
-                    .document(currentDate).collection("foodAdded").add(food)
-                //add to today's data and update to firebase
-                db.collection("users").document(uid).collection("dates")
-                    .document(currentDate).get().addOnSuccessListener { documentSnapshot ->
-                        var dailyData= documentSnapshot.toObject(DailyInfo::class.java)
-                        if (dailyData!=null)
-                            dailyData=
-                                DailyInfo(dailyData.date,dailyData.calories+food.calories,dailyData.carb+food.carb,
-                                    dailyData.fat+food.fat,dailyData.sugars+food.sugars,dailyData.protein+food.protein)
-                        db.collection("users").document(uid).collection("dates")
-                            .document(currentDate).set(dailyData!!)
-                    }
-
-            }
-        }
-
-
     }
 }
 
-class FoodAddedAdapter(private val list : ArrayList<Food>, private val listString : ArrayList<String>)
-    : RecyclerView.Adapter<FoodAddedViewHolder>(), Filterable {
-
-    private var filteredFoodList = ArrayList<Food>()
-    private val expandedPositionSet : HashSet<Int> = HashSet()
-    private val db= FirebaseFirestore.getInstance()
-    private val uid= FirebaseAuth.getInstance().uid
-
-    init {
-        filteredFoodList = list
-    }
+class FoodAddedAdapter(private val list : ArrayList<Food>)
+    : RecyclerView.Adapter<FoodAddedViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodAddedViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return FoodAddedViewHolder(inflater, parent)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: FoodAddedViewHolder, position: Int) {
-        holder.bind(filteredFoodList[position],db, LocalDate.now().toString(),uid)
-        holder.itemView.childFoodView.text = "CHILD"
-
-        holder.itemView.expandableFood.setOnExpandListener(object : ExpandableLayout.OnExpandListener {
-            override fun onExpand(expanded: Boolean) {
-                if (expandedPositionSet.contains(position)) {
-                    expandedPositionSet.remove(position)
-                }
-                else {
-                    expandedPositionSet.add(position)
-                }
-            }
-        })
+        holder.bind(list[position])
     }
 
     override fun getItemCount(): Int {
-        return filteredFoodList.size
-    }
-
-    override fun getFilter(): Filter {
-        return object : Filter() {
-
-            override fun performFiltering(constraint: CharSequence?): FilterResults {
-                val charSearch = constraint.toString()
-                if (charSearch.isEmpty()) {
-                    filteredFoodList = list
-                }
-                else {
-                    val filteredResults = ArrayList<Food>()
-                    for (row in list) {
-                        if (row.name.toLowerCase(Locale.ROOT).contains(charSearch.toLowerCase(Locale.ROOT))) {
-                            filteredResults.add(row)
-                        }
-                    }
-                    filteredFoodList = filteredResults
-                }
-                val finalFiltered = FilterResults()
-                finalFiltered.values = filteredFoodList
-                return finalFiltered
-
-            }
-
-            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
-                filteredFoodList = results?.values as ArrayList<Food>
-                notifyDataSetChanged()
-            }
-        }
+        return list.size
     }
 }
--- app/src/main/java/com/example/washugains/Fragment/ProgressFragments/AddedFoodFragment.kt
+++ app/src/main/java/com/example/washugains/Fragment/ProgressFragments/AddedFoodFragment.kt
@@ -1,25 +1,30 @@
 package com.example.washugains.Fragment.ProgressFragments
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.washugains.Adapter.FoodAdapter
+import com.example.washugains.Adapter.FoodAddedAdapter
+import com.example.washugains.DataClass.DailyInfo
 import com.example.washugains.DataClass.Food
 import com.example.washugains.R
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.sakebook.android.library.multilinedevider.MultiLineDivider
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.food_fragment.*
+import java.time.LocalDate
 
 class AddedFoodFragment: Fragment() {
-
+    val foodList : ArrayList<Food> = ArrayList()
+    private val db:FirebaseDatabase= FirebaseDatabase.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,50 +34,26 @@ class AddedFoodFragment: Fragment() {
         return inflater.inflate(R.layout.added_food_fragment, container, false)
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//
-//
-//        val recyclerView = view?.findViewById<RecyclerView>(R.id.foodAddedRecyclerView)
-//        val adapter = FoodAdapter(foodList, foodString)
-//        recyclerView?.adapter = adapter
-//        recyclerView?.layoutManager = LinearLayoutManager(context)
-//        val multiLineDivider = MultiLineDivider(context!!)
-//        recyclerView?.addItemDecoration(multiLineDivider)
-//
-//        val ref = FirebaseDatabase.getInstance().getReference("food")
-//        ref.addValueEventListener(object : ValueEventListener {
-//
-//            override fun onDataChange(dataSnapshot: DataSnapshot) {
-//                foodList.clear()
-//                for (productSnapshot in dataSnapshot.children) {
-//                    val food = productSnapshot.getValue(Food::class.java)
-//                    if(food!=null) {
-//                        foodList.add(food)
-//                        foodString.add(food.name)
-//                    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStart() {
+        super.onStart()
+
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.foodAddedRecyclerView)
+        val adapter = FoodAddedAdapter(foodList)
+        recyclerView?.adapter = adapter
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+        val multiLineDivider = MultiLineDivider(context!!)
+        recyclerView?.addItemDecoration(multiLineDivider)
+
+        val uid=FirebaseAuth.getInstance().uid
+//        if(uid!=null) {
+//            db.collection("users").document(uid).collection("dates")
+//                .document(LocalDate.now().toString()).get()
+//                .addOnSuccessListener { documentSnapshot ->
+//                    var food = documentSnapshot.toObject(Food::class.java)
+//                    food.id = documentSnapshot.id
 //                }
-//                adapter.notifyDataSetChanged()
-//            }
-//
-//            override fun onCancelled(p0: DatabaseError) {
-//                throw p0.toException()
-//            }
-//        })
-//
-//        //grabs searchView from food_fragment
-//        foodSearch = food_search
-//        foodSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-//
-//            override fun onQueryTextChange(newText: String?): Boolean {
-//                adapter.filter.filter(newText)
-//                return false
-//            }
-//
-//            override fun onQueryTextSubmit(query: String?): Boolean {
-//                return false
-//            }
-//        })
-//
-//    }
+//        }
+
+    }
 }
